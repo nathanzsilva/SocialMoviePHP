5e8f69a8870c87b17f89ffd3c43607435a1e8531
Nathan Silva <nathan.silva26@gmail.com>
2023-12-07 21:57:23 -0300

    update

---
+++ comunidade.php
@@ -0,0 +1,160 @@
+<?php
+session_start();
+include_once("conectaDB.php");
+
+
+$idFilme = $_GET["id"] ?? "";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidade de Filmes - Detalhes do Filme</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body>
+    <div class="body-comunidade">
+        <header>
+            <img src="./images/logo.png" alt="Logo">
+            <div>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li class="active">Comunidade</li>
+                    <li>Perfil</li>
+                </ul>
+            </div>
+            <div class="row aic gap5">
+                <div onmouseenter='seeExit()' onmouseleave='hideExit()'>
+                    <?php
+
+                    $user = $_SESSION['userName'] ?? "";
+                    if($user != "") {
+                        $userName = $_SESSION['userName'];
+                        if($userName != null)
+                            echo "<p>$userName</p>";
+                    }
+                    ?>
+
+                    <a href="./deslogar.php" style="display:none;" id="deslogar">
+                        Deslogar
+                    </a>
+                </div>
+
+                <?php
+                if($idFilme == "") {
+                    echo "<a href='./insertPost.php'><button class='btn-post'>Post</button></a>";
+                } else {
+                    echo "<a href='./insertPost.php?id=".$idFilme."'><button class='btn-post'>Post</button></a>";
+
+                }
+                ?>
+
+            </div>
+        </header>
+
+        <div class="movie-details">
+            <div class="movie">
+                <?php
+                if($idFilme != "") {
+                    $query = "select * from Filmes where codigo = $idFilme;";
+
+                    $result = mysqli_query($mysqli, $query);
+
+                    $row = mysqli_fetch_assoc($result);
+                    $img = $row["urlImagem"];
+                    $descricao = $row["descricao"];
+                    echo "<img src='$img'>";
+
+                    echo "<div class='movie-info'><h2>$descricao</h2></div>";
+
+                }
+
+                ?>
+            </div>
+        </div>
+        <?php
+        if($idFilme != "") {
+            echo "<hr class='margin20'>";
+        } ?>
+        <div class="container-comments">
+            <h2>Comentários</h2>
+
+
+            <?php
+            if($idFilme != "") {
+
+                $query = "select * from Posts as p join Usuario as u on p.usuariocodigo = u.codigo where codigoFilme =$idFilme;";
+
+                $result = mysqli_query($mysqli, $query);
+
+                if(mysqli_num_rows($result) > 0) {
+                    foreach($result as $row) {
+
+                        $username = $row["nome"];
+                        $texto = $row["texto"];
+                        $dataCad = $row["dataCad"];
+                        $formattedDate = date("d/m/Y", strtotime($dataCad));
+
+
+                        echo "  <div class='comment'><div class='user-info'>";
+                        echo "<h3 class='username'>$username - $formattedDate</h3>";
+                        echo "</div>";
+
+                        echo "<p class='comment-text'>$texto</p>";
+
+                    }
+                } else {
+                    echo "<br>";
+                    echo "<h3>Sem nenhum comentário</h3>";
+                }
+
+            } else {
+                $query = "select *, f.descricao as filme from Posts as p join Usuario as u on p.usuariocodigo = u.codigo join Filmes as f on f.codigo = p.codigofilme;";
+
+                $result = mysqli_query($mysqli, $query);
+
+                foreach($result as $row) {
+
+                    $username = $row["nome"];
+                    $filme = $row["filme"];
+                    $texto = $row["texto"];
+                    $dataCad = $row["dataCad"];
+                    $formattedDate = date("d/m/Y", strtotime($dataCad));
+
+
+                    echo "  <div class='comment'><div class='user-info'>";
+                    echo "<h3 class='username'>$username - $filme - $formattedDate</h3>";
+                    echo "</div>";
+
+                    echo "<p class='comment-text'>$texto</p>";
+
+                    echo "</div>";
+
+                }
+            }
+
+            ?>
+
+
+
+
+        </div>
+
+
+    </div>
+
+
+    </div>
+
+    <script src="./scripts/script.js"></script>
+    <script src="./scripts/exit.js"></script>
+    <script>
+        // Adicione qualquer lógica adicional necessária para processar os comentários
+    </script>
+</body>
+
+</html>
--- conectaDB.php
+++ conectaDB.php
@@ -1,5 +1,19 @@
 <?php
-  $mysqli = mysqli_init();
-  $mysqli->ssl_set(NULL, NULL, "./cacert.pem", NULL, NULL);
-  $mysqli->real_connect("aws.connect.psdb.cloud", "as5zonq8yruds8pa3h9y", "pscale_pw_vaKXJi90IFcAJZM5doWLE7d7PNGoRpcumzxI6aoejYu", "bancoetecantoniofurlan");  
+// $server = "aws.connect.psdb.cloud";
+// $user = "n3ohvvk5d4lki8f7uose";
+// $password = "pscale_pw_yb3JYUeRKJyg6RbgH0KxIQ4et6XGaaEIwxypEmK7fll";
+// $database = "bancoetecantoniofurlan";
+
+// $mysqli = mysqli_init();
+// $mysqli->ssl_set(NULL, NULL, "./cacert.pem", NULL, NULL);
+// $mysqli->real_connect($server, $user, $password, $database);
+
+$server = "127.0.0.1";
+$user = "root";
+$password = "root";
+$database = "bancoetecantoniofurlan";
+
+
+$mysqli = mysqli_init();
+$mysqli->real_connect($server, $user, $password, $database);
 ?>
